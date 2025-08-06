889fd5b762e1c90b067a51f59d59f24fb3581722
programanda03 <paganiamanda791@gmail.com>
2025-08-06 13:16:47 -0300

    refactor: rename entities and update package structure to improve clarity
    # Conflicts:
    #	internal/domain/entities/repository/userModel.go
    #	internal/domain/model/dto/user_dto.go
    #	internal/domain/repository/parts_supply_repo.go
    #	internal/domain/repository/service_repo.go
    #	internal/domain/repository/user-example/user.go
    #	internal/domain/repository/user_repo.go
    #	internal/domain/repository/vehicle_repo.go
    #	internal/domain/user-example/user.go
    #	internal/domain/user/user.go
    #	internal/infrastructure/databse/dbConnection.go
    #	internal/infrastructure/http/middleware/proxies.go
    #	internal/infrastructure/http/routes/routes.go
    #	internal/infrastructure/http/routes/user.go
    #	internal/infrastructure/http/user_handler.go
    #	internal/infrastructure/http/user_handler_test.go
    #	internal/infrastructure/middleware/dbConnection.go
    #	internal/infrastructure/middleware/proxies.go
    #	internal/middleware/dbConnection.go
    #	internal/middleware/proxies.go

---
+++ internal/domain/repository/additional_repair_repo.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"mecanica_xpto/internal/domain/model/entities"
+	"mecanica_xpto/internal/domain/model/valueobject"
+)
+
+// 1:N relationship between AdditionalRepair and AdditionalRepairStatus
+type AdditionalRepairStatusDTO struct {
+	ID                uint   `gorm:"primaryKey"`
+	Description       string `gorm:"size:50;not null"`
+	AdditionalRepairs []AdditionalRepairDTO
+}
+
+func (arsm AdditionalRepairStatusDTO) ToDomain() entities.AdditionalRepairStatus {
+	return entities.AdditionalRepairStatus{
+		ID:                arsm.ID,
+		Description:       arsm.Description,
+		AdditionalRepairs: nil, // This will be populated later if needed
+	}
+}
+
+type AdditionalRepairDTO struct {
+	ID             uint                      `gorm:"primaryKey"`
+	ServiceOrderID uint                      `gorm:"not null"`
+	ServiceOrder   ServiceOrderDTO           `gorm:"foreignKey:ServiceOrderID"`
+	ServiceID      uint                      `gorm:"not null"`
+	Service        ServiceDTO                `gorm:"foreignKey:ServiceID"`
+	PartsSupplyID  uint                      `gorm:"not null"`
+	PartsSupply    PartsSupplyDTO            `gorm:"foreignKey:PartsSupplyID"`
+	ARStatusID     uint                      `gorm:"not null"`
+	ARStatus       AdditionalRepairStatusDTO `gorm:"foreignKey:ARStatusID"`
+}
+
+func (arm *AdditionalRepairDTO) ToDomain() entities.AdditionalRepair {
+	return entities.AdditionalRepair{
+		ID:           arm.ID,
+		ServiceOrder: arm.ServiceOrder.ToDomain(),
+		Service:      arm.Service.ToDomain(),
+		PartsSupply:  arm.PartsSupply.ToDomain(),
+		ARStatus:     valueobject.ParseAdditionalRepairStatus(arm.ARStatus.Description),
+	}
+}
---
+++ internal/domain/repository/customer_repo.go
@@ -0,0 +1,38 @@
+package repository
+
+import (
+	"mecanica_xpto/internal/domain/model/entities"
+	"mecanica_xpto/internal/domain/model/valueobject"
+)
+
+// 1:1 relationship between Customer and User
+// 1:N relationship between Customer and Vehicle
+// 1:N relationship between Customer and ServiceOrder
+type CustomerDTO struct {
+	ID            uint     `gorm:"primaryKey"`
+	UserID        uint     `gorm:"unique;not null"`
+	User          *UserDTO `gorm:"foreignKey:UserID;references:ID"`
+	CpfCnpj       string   `gorm:"size:20;not null"`
+	PhoneNumber   string   `gorm:"size:20;not null"`
+	FullName      string   `gorm:"size:100;not null"`
+	Vehicles      []VehicleDTO
+	ServiceOrders []ServiceOrderDTO
+}
+
+func (cm *CustomerDTO) ToDomain() entities.Customer {
+	var user *entities.User
+	if cm.User != nil {
+		u := cm.User.ToDomain()
+		user = &u
+	}
+	return entities.Customer{
+		ID:            cm.ID,
+		UserID:        cm.UserID,
+		User:          user,
+		CpfCnpj:       valueobject.ParseCPF_CNPJ(cm.CpfCnpj),
+		PhoneNumber:   cm.PhoneNumber,
+		FullName:      cm.FullName,
+		Vehicles:      nil, // This will be populated later if needed
+		ServiceOrders: nil, // This will be populated later if needed
+	}
+}
