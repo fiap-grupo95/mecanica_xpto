8fb98f9eb3a93e0011e8f5276768341a7419f605
programanda03 <paganiamanda791@gmail.com>
2025-08-06 13:16:47 -0300

    refactor: rename entities and update package structure to improve clarity
    # Conflicts:
    #	internal/domain/entities/repository/userModel.go
    #	internal/domain/model/dto/user_dto.go
    #	internal/domain/repository/parts_supply_repo.go
    #	internal/domain/repository/service_repo.go
    #	internal/domain/repository/user-example/user.go
    #	internal/domain/repository/user_repo.go
    #	internal/domain/repository/vehicle_repo.go
    #	internal/domain/user-example/user.go
    #	internal/domain/user/user.go
    #	internal/infrastructure/databse/dbConnection.go
    #	internal/infrastructure/http/middleware/proxies.go
    #	internal/infrastructure/http/routes/routes.go
    #	internal/infrastructure/http/routes/user.go
    #	internal/infrastructure/http/user_handler.go
    #	internal/infrastructure/http/user_handler_test.go
    #	internal/infrastructure/middleware/dbConnection.go
    #	internal/infrastructure/middleware/proxies.go
    #	internal/middleware/dbConnection.go
    #	internal/middleware/proxies.go

---
+++ internal/domain/repository/vehicle_repo.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"mecanica_xpto/internal/domain/model/entities"
+	"mecanica_xpto/internal/domain/model/valueobject"
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type VehicleDTO struct {
+	ID            uint           `gorm:"primaryKey"`
+	Plate         string         `gorm:"size:10;not null"`
+	CustomerID    uint           `gorm:"not null"`
+	Customer      CustomerDTO    `gorm:"foreignKey:CustomerID"`
+	Model         string         `gorm:"size:50;not null"`
+	Year          string         `gorm:"size:4"`
+	Brand         string         `gorm:"size:50;not null"`
+	CreatedAt     time.Time      `gorm:"autoCreateTime"`
+	UpdatedAt     time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt     gorm.DeletedAt `gorm:"index"`
+	ServiceOrders []ServiceOrderDTO
+}
+
+func (v *VehicleDTO) ToDomain() entities.Vehicle {
+	return entities.Vehicle{
+		ID:        v.ID,
+		Plate:     valueobject.ParsePlate(v.Plate),
+		Customer:  v.Customer.ToDomain(),
+		Model:     v.Model,
+		Year:      v.Year,
+		Brand:     v.Brand,
+		CreatedAt: v.CreatedAt,
+		UpdatedAt: v.UpdatedAt,
+		DeletedAt: func() *time.Time {
+			if v.DeletedAt.Valid {
+				return &v.DeletedAt.Time
+			}
+			return nil
+		}(),
+		ServiceOrders: nil, // This will be populated by the repository layer
+	}
+}
